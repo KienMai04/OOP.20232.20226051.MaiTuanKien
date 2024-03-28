f42e4e3f3927f5e1530f66a60200ff24be15c0ab
KienMai04 <maituankien1115@gmail.com>
2024-03-29 00:09:53 +0700

    Change file in assignment folder

--- Lab01/ChoosingOptions.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class ChoosingOptions {
-    public static void main(String[] args) {
-        int option = JOptionPane.showConfirmDialog(null, "Do you want to change to first class ticket?");
-        JOptionPane.showMessageDialog(null, "you have choosen: " + (option == JOptionPane.OK_OPTION ? "Yes" : "No"));
-
-    }
-}
